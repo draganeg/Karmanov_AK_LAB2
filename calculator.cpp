e803ca89bd752c1e9ab53620f928963c71bc85ed
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:28:04 +0200

    fix truncation error
    To convert float to integer the truncation is performed, but the
    rounding is expected.
    
    Test: Add (4.9, 4.9) should return 10.
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>
    
    formatting: use tabs instead of spaces
    
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- calculator.cpp
+++ calculator.cpp
@@ -2,7 +2,23 @@
 
 int Calculator::Add (double a, double b)
 {
+<<<<<<< HEAD
+<<<<<<< HEAD
 	return a + b + 0.5;
+=======
+=======
+>>>>>>> d20b996 (formatting: use tabs instead of spaces)
+<<<<<<< HEAD
+=======
+	return a + b + 0.5;
+>>>>>>> 4ad40a3 (fix truncation error)
+<<<<<<< HEAD
+>>>>>>> ff7f59a (fix truncation error)
+=======
+=======
+	return a + b;
+>>>>>>> cbcb06d (formatting: use tabs instead of spaces)
+>>>>>>> d20b996 (formatting: use tabs instead of spaces)
 }
 
 int Calculator::Sub (double a, double b)
